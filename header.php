9bc5bc6103dfbc57f0bd827ef466efeaa713ab0b
A.Forooghi <96820237+AboalfazlForooghi2004@users.noreply.github.com>
2022-03-08 23:51:46 +0330

    Create header.php

---
+++ header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+<meta charset="utf-8">
+
+<title>header_nav</title>
+
+</head>
+
+<body>
+
+<nav  style="text-align:center" >
+
+<ul>
+
+<li><a hrf="Home.php">صفحه اصلی</a></li>
+
+<li><a hrf=registr.php>عضویت در سایت</a></li>
+
+<li><a hrf=#>ورود</a></li> 
+
+<li><a hrf=#>ارتباط با ما </a></li>
+
+</ul>
+
+</nav>
+
+</body>
+
+</html>
