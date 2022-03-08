d7643dc551dbc7cfb2179306eec92c5e1ddd3b69
A.Forooghi <96820237+AboalfazlForooghi2004@users.noreply.github.com>
2022-03-08 23:56:13 +0330

    Create register.php

---
+++ register.php
@@ -0,0 +1,84 @@
+<!doctype html>
+
+<html  dir="rtl">
+
+<head>
+
+<meta charset="utf-8">
+
+<title>فرم عضویت </title>
+
+</head>
+
+<body style="background-color:#C80003>
+
+<?php
+
+include ("header.php");
+
+?>
+
+<br>
+
+<center>
+
+<table width="362" height="395" border="1" cellpadding="0" cellspacing="0">
+
+ <tbody>
+
+    <tr>
+
+      <td style="#7E7E7E">
+
+<form  action="Check.php" id="form1" name="form1" method="post">
+
+          نام و نام خانوادگی :
+
+          <input type="text" name="text1" id="text1">
+
+کلمه عبور :
+
+          <input type="password" name="password" id="password">
+
+         تکرار کلمه عبور:
+
+          <input type="password" name="repassword" id="repassword">
+
+        <p>&nbsp; </p>
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+        <p>&nbsp;</p>
+
+          <input name="Send" type="submit" value="Send">
+
+      </form>
+
+</td>
+
+    </tr>
+
+  </tbody>
+
+</table>
+
+</center>
+
+</body>
+
+</html>
+
+ 
+
