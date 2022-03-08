47ebdee7306665470d013a1ace7c0246f6564f3e
A.Forooghi <96820237+AboalfazlForooghi2004@users.noreply.github.com>
2022-03-08 23:53:34 +0330

    Create Home.php

---
+++ Home.php
@@ -0,0 +1,27 @@
+!doctype html>
+
+<html>
+
+<head>
+
+<meta charset="utf-8">
+
+<title>صفحه اصلی</title>
+
+</head>
+
+<body>
+
+<?php
+
+include ("header.php");
+
+?>
+
+<br>
+
+<h2style="text-align:center>Porojeh_php</h2>
+
+</body>
+
+</html>
